9dab2031e2d7726a61283ee855514c4187f56aa3
avdyl123 <avdylzeqiri732@gmail.com>
2025-05-05 02:59:16 -0700

    a
    a

---
+++ index1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>    
+<h1>HELLO</h1>
+<p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique doloribus excepturi sequi expedita eligendi labore?</p>
+
+<?php
+    echo "This is my first page with PHP"
+?>
+
+</body>
+</html>

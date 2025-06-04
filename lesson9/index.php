c32282ba6f7f608b688bbef459b0c4d9d267262b
avdyl123 <avdylzeqiri732@gmail.com>
2025-06-04 02:28:43 -0700

    A
    A

---
+++ lesson9/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+    <head></head>
+    <body>
+        <form action="add.php" method="POST">
+            <input type="text" name="name" placeholder="Name"><br>
+            <input type="text" name="surname" placeholder="Surname"><br>
+            <input type="email" name="email" placeholder="Email"><br>
+            <input type="submit" name="submit" placeholder="Submit"><br>
+            <button type="submit" name="submit">ADD</button>
+        </form>
+    </body>
+
+
+</html>
